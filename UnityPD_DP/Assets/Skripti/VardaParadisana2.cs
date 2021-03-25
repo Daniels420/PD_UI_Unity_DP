9a846e227f9ecd9d3fe2a28cb1eef9c27915f273
Daniels <nezinu69@gmail.com>
2021-03-25 19:58:55 +0200

    Izveidota vēl viena pogu kategorija,tēla apģērba panelis pabeigts,uzsākts dropdown veidošana

--- UnityPD_DP/Assets/Skripti/VardaParadisana2.cs
+++ UnityPD_DP/Assets/Skripti/VardaParadisana2.cs
@@ -3,7 +3,7 @@ using System.Collections;
 //Jāpievieno namespace, lai varētu lietot UI elementus.
 using UnityEngine.UI;
 
-public class VardaParadisana : MonoBehaviour {
+public class VardaParadisana2 : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         //Ja grib tekstu, kas parādās jau rakstīšanas brīdī, tad kodu no uzglabatTekstu liek te.
@@ -19,7 +19,6 @@ public class VardaParadisana : MonoBehaviour {
     public void uzglabaTekstu(){
         //No komponentes IevadesLauks1 iegūst pakārtoto Text komponenti
         teksts = ievadesLauks.GetComponent<Text>().text;
-        tekstaAttelosana.GetComponent<Text>().text = "Tavs izvēlētais vārds ir: "+teksts.ToUpper()+"!";
+        tekstaAttelosana.GetComponent<Text>().text = "Tev ir: "+teksts.ToUpper()+" gadi!";
     }
-}   
-
+}  
