82d01ce508a161d0e7687641d2bcc4caa149c5ae
Daniels <nezinu69@gmail.com>
2021-03-25 09:45:16 +0200

    Pievienoju pogas,kur jautā tēla Vārdu un Vecumu

---
+++ UnityPD_DP/Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+//Jāpievieno namespace, lai varētu lietot UI elementus.
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour {
+    // Update is called once per frame
+    void Update () {
+        //Ja grib tekstu, kas parādās jau rakstīšanas brīdī, tad kodu no uzglabatTekstu liek te.
+    }
+
+    //Teksts ko rakstīs
+    public string teksts;
+    //Lauks kurā ierakstīs tekstu
+    public GameObject ievadesLauks;
+    //Kur attēlos tekstu
+    public GameObject tekstaAttelosana;
+
+    public void uzglabaTekstu(){
+        //No komponentes IevadesLauks1 iegūst pakārtoto Text komponenti
+        teksts = ievadesLauks.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = "Tevis izvēlētie iestatījumi: "+teksts.ToUpper()+"!";
+    }
+}   
+
