109cd81cf7a2d8a90093fdf08bdf4de054f6d654
Daniels <nezinu69@gmail.com>
2021-03-30 20:57:51 +0300

    Pievienota fona un pogu mūzika

---
+++ UnityPD_DP/Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtteluParadisana : MonoBehaviour{
+    public GameObject varonis1;
+    public GameObject varonis2;
+    public GameObject varonis3;
+    public GameObject varonis4;
+    public void varona1Attelosana(bool vertiba){
+        varonis1.SetActive(vertiba);
+    }
+        public void varona2Attelosana(bool vertiba){
+        varonis2.SetActive(vertiba);
+        }
+        public void varona3Attelosana(bool vertiba){
+        varonis3.SetActive(vertiba);
+        }
+
+    public void varona4Attelosana(bool vertiba){
+        varonis4.SetActive(vertiba);
+            
+            }
+        }   
+    
+
+
+
---
+++ UnityPD_DP/Assets/Skripti/DrebjuParadisana.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrebjuParadisana : MonoBehaviour {
+    public GameObject CepuresIzvele;
+    public GameObject BrunuIzvele;
+    public GameObject KurpesIzveleK;
+    public GameObject KurpesIzveleL;
+    public GameObject ElkonaIzveleK;
+    public GameObject ElkonaIzveleL;
+    public GameObject PlecsK;
+    public GameObject PlecsL;
+    public GameObject Bikses;
+    public GameObject IerociL;
+    public GameObject IerociK;
+
+    public void CepuresIzveleAttelosana(bool vertiba){
+        CepuresIzvele.SetActive(vertiba);
+    }
+    public void BrunuIzveleAttelosana(bool vertiba){
+        BrunuIzvele.SetActive(vertiba);
+    }
+    public void KurpesIzveleKAttelosana(bool vertiba){
+        KurpesIzveleK.SetActive(vertiba);
+    }
+        public void KurpesIzveleLAttelosana(bool vertiba){
+        KurpesIzveleL.SetActive(vertiba);
+        }
+        public void ElkonaIzveleKAttelosana(bool vertiba){
+        ElkonaIzveleK.SetActive(vertiba);
+        }
+        public void ElkonaIzveleLAttelosana(bool vertiba){
+        ElkonaIzveleL.SetActive(vertiba);
+        }
+        public void PlecsKAttelosana(bool vertiba){
+        PlecsK.SetActive(vertiba);
+        }
+        public void PlecsLAttelosana(bool vertiba){
+        PlecsL.SetActive(vertiba);
+        }
+        public void BiksesAttelosana(bool vertiba){
+        Bikses.SetActive(vertiba);
+        }
+        public void IerociKAttelosana(bool vertiba){
+        IerociK.SetActive(vertiba);
+        }
+        public void IerociLAttelosana(bool vertiba){
+        IerociL.SetActive(vertiba);
+        }
+
+}
+
